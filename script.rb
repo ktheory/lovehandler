797c14d7ef1c0a9ed777619a21dd90752247a224
Aaron Suggs <aaron@ktheory.com>
2015-05-11 17:39:26 -0400

    wip

--- script.rb
+++ script.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
-first = File.readlines('first').map(&:chomp).each{|word| word[0] = word[0].upcase}
-middle = File.readlines('middle').map(&:chomp).each{|word| word[0] = word[0].upcase}
-last = File.readlines('last').map(&:chomp).each{|word| word[0] = word[0].upcase}
+first = File.readlines('first.txt').map(&:chomp).each{|word| word[0] = word[0].upcase}
+middle = File.readlines('middle.txt').map(&:chomp).each{|word| word[0] = word[0].upcase}
+last = File.readlines('last.txt').map(&:chomp).each{|word| word[0] = word[0].upcase}
 
 puts 20.times.map{ 
   [
     first[rand(first.size)],
-    rand(2) == 1 ? middle[rand(middle.size)] : nil,
+    rand(4) != 0 ? middle[rand(middle.size)] : nil,
     last[rand(last.size)]
   ].compact.join(" ")
 }
